80bc78c1c5757230ae7e63d9bf66328e22bc8b40
sgb128 <71208251+sgb128@users.noreply.github.com>
2020-10-26 22:16:42 +0300

    Commit homework lesson 1 Homework is done.

--- homeworks/lesson1/task4.py
+++ homeworks/lesson1/task4.py
@@ -1,11 +1,13 @@
-v_prompt = 'Введите целое положительное число: '
-v_result = 'Самая большая цифра в веденном числе: {0}'
-v_str_positive_number = str(input(v_prompt))
-iterator = 0
-result = -1
-while iterator < len(v_str_positive_number):
-    if int(v_str_positive_number[iterator]) > result:
-        result = int(v_str_positive_number[iterator])
-    iterator += 1
-
-print(v_result.format(result))
+vs_prompt = 'Введите целое положительное число: '
+vs_result = 'Самая большая цифра в веденном числе: {0}'
+vs_iterator = 'Количество циклов выполнено: {0}'
+vpn_int_positive_number = int(input(vs_prompt))
+vn_iterator = 0
+vn_result = -1
+while vpn_int_positive_number and vn_result != 9:
+    if vn_result < vpn_int_positive_number % 10:
+        vn_result = vpn_int_positive_number % 10
+    vn_iterator += 1
+    vpn_int_positive_number //= 10
+print(vs_result.format(vn_result))
+print(vs_iterator.format(vn_iterator))
