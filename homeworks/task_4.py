b5fd0519e58d9a5b7fd8ae3787bb5e5a6ef2692c
sgb128 <71208251+sgb128@users.noreply.github.com>
2021-03-02 02:13:16 +0300

    Homework task_4 complete. Commit homework lesson 2

--- homeworks/task_4.py
+++ homeworks/task_4.py
@@ -2,3 +2,20 @@
 Посчитать, сколько раз встречается определенная цифра в введенной последовательности чисел.
 Количество вводимых чисел и цифра, которую необходимо посчитать, задаются вводом с клавиатуры.
 """
+def cnt_numb(fnum, i, num = 0):
+    if num != 0:
+        if num%10 == fnum:
+            return 1 + cnt_numb(fnum, i, num//10)
+        else:
+            return cnt_numb(fnum, i, num // 10)
+    else:
+        if i > 0:
+            a = int(input('Введите число:'))
+            return cnt_numb(fnum, i - 1, a)
+        else:
+            return 0
+
+cnt = int(input('Введите количество чисел:'))
+find_num = int(input('Введите искомое число:'))
+rez = cnt_numb(find_num, cnt)
+print(rez)
